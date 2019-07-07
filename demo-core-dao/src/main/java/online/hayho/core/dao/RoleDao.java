af7d35410a782b35447d8121447b01086ddfa2e3
nockasdd <nockasdd@gmail.com>
2019-07-07 21:07:36 +0700

    Delect

--- demo-core-dao/src/main/java/online/hayho/core/dao/RoleDao.java
+++
@@ -1,7 +0,0 @@
-package online.hayho.core.dao;
-
-import online.hayho.core.data.dao.GenericDao;
-import online.hayho.core.persistence.entity.RoleEntity;
-
-public interface RoleDao extends GenericDao<Integer , RoleEntity> {
-}
